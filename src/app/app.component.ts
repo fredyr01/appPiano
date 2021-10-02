0721184fbdb7c4b70a710aa3e3ceaf5d3335136b
fredyr01 <fredyrios01@gmail.com>
2021-10-02 11:16:21 -0500

    Piano en angular!

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+
+  aplicarSonido(numero:number): void{
+
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/nota'+numero+'.mp3';
+    audio.load();
+    audio.play();
+
+  }
+}
